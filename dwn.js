76acf8700915b5c1f7e28662472c611fd35ff667
aksanza <ajisaaah@gmail.com>
2020-01-27 16:08:08 +0700

    Delete dwn.js

--- dwn.js
+++
@@ -1,6 +0,0 @@
-//<![CDATA[
-	var yb = gVar()["yb"];
-	if(yb && yb != 'undefined'){mKifrm(yb)}
-	var target_download = 'https://shewantstoflyawaxxxy.blogspot.com/p/down.html';
-	doing('download_btn',target_download);
-//]]>
