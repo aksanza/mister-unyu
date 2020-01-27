7f9e75572dfdd108ff12ebe94fd8d40b3500fdce
aksanza <ajisaaah@gmail.com>
2020-01-27 16:10:03 +0700

    Update down.js

--- down.js
+++ down.js
@@ -1,6 +1,6 @@
 //<![CDATA[
 	var yb = gVar()["yb"];
 	if(yb && yb != 'undefined'){mKifrm(yb)}
-	var target_download = 'https://shewantstoflyawaxxxy.blogspot.com/p/down.html';
+	var target_download = 'https://shewantstoflyaway.blogspot.com/p/down.html';
 	doing('download_btn',target_download);
 //]]>
